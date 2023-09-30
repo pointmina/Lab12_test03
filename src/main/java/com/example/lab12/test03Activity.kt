ac4d8a004fd05d5f80bb397565fa55170e3dacf6
pointmina <pointmina@gmail.com>
2023-10-01 00:57:16 +0900

    test01

---
+++ src/main/java/com/example/lab12/test03Activity.kt
@@ -0,0 +1,12 @@
+package com.example.lab12
+//네비게이션뷰
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class test03Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test03)
+    }
+}
